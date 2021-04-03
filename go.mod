3ae591bcaece8372509ef4667e4bd4ea2c847b23
Dovar <dovarabdulhanonov@gmail.com>
2021-04-03 20:34:55 +0500

    v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Dovar001/bank
+module github.com/Dovar001/bank/v2
 
 go 1.16
--- pkg/types/types.go
+++ pkg/types/types.go
@@ -8,11 +8,21 @@ type Money int64
 //Category  представляет сабой категорию в которой был совершен платёж
 type Category string
 
+//Status представляет собой статус платежа
+type Status string
+
+//Предопределлённые статуси платежей
+const (
+	StatusOK Status = "OK"
+	StatusFail Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"		
+)
+
 
 //Payment представляет  информацию о платеже  
 type Payment struct {
 	ID int
 	Amount Money
 	Category Category
-	
+	Status Status 
 }
